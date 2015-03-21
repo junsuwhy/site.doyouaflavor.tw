643fa3bbdb2e8ec62ba06a7c577b06b9ba8a0796
junsuwhy <junsuwhy@gmail.com>
2015-03-21 12:39:01 +0800

    Upload to server.

--- sites/all/themes/zen/templates/page.tpl.php
+++ sites/all/themes/zen/templates/page.tpl.php
@@ -12,6 +12,7 @@
 
   <header class="header" id="header" role="banner">
 
+    <?php print render($page['header']); ?>
     <?php if ($logo): ?>
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
     <?php endif; ?>
@@ -45,8 +46,6 @@
         )); ?>
       </nav>
     <?php endif; ?>
-
-    <?php print render($page['header']); ?>
 
   </header>
 
