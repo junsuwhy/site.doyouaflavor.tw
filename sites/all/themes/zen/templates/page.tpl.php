210e9a83536d042ef3ced734a9cb714dc48a15ea
junsuwhy <junsuwhy@gmail.com>
2015-03-24 21:02:02 +0800

    Add css file.

--- sites/all/themes/zen/templates/page.tpl.php
+++ sites/all/themes/zen/templates/page.tpl.php
@@ -17,35 +17,34 @@
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
     <?php endif; ?>
 
-    <?php if ($site_name || $site_slogan): ?>
-      <div class="header__name-and-slogan" id="name-and-slogan">
-        <?php if ($site_name): ?>
-          <h1 class="header__site-name" id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
-        <?php endif; ?>
-
-        <?php if ($site_slogan): ?>
-          <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
-        <?php endif; ?>
-      </div>
-    <?php endif; ?>
-
-    <?php if ($secondary_menu): ?>
-      <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
-        <?php print theme('links__system_secondary_menu', array(
-          'links' => $secondary_menu,
-          'attributes' => array(
-            'class' => array('links', 'inline', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => $secondary_menu_heading,
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
-      </nav>
-    <?php endif; ?>
+<div class="navigation-wrapper">
+  <div id="navigation">
+  
+       <?php if ($main_menu): ?>
+         <nav id="main-menu" role="navigation" tabindex="-1">
+           <?php
+           // This code snippet is hard to modify. We recommend turning off the
+           // "Main menu" on your sub-theme's settings form, deleting this PHP
+           // code block, and, instead, using the "Menu block" module.
+           // @see https://drupal.org/project/menu_block
+           print theme('links__system_main_menu', array(
+             'links' => $main_menu,
+             'attributes' => array(
+               'class' => array('links', 'inline', 'clearfix'),
+             ),
+             'heading' => array(
+               'text' => t('Main menu'),
+               'level' => 'h2',
+               'class' => array('element-invisible'),
+             ),
+           )); ?>
+         </nav>
+       <?php endif; ?>
+  
+       <?php print render($page['navigation']); ?>
+  
+     </div>
+</div>
 
   </header>
 
@@ -70,32 +69,7 @@
       <?php print $feed_icons; ?>
     </div>
 
-    <div id="navigation">
-
-      <?php if ($main_menu): ?>
-        <nav id="main-menu" role="navigation" tabindex="-1">
-          <?php
-          // This code snippet is hard to modify. We recommend turning off the
-          // "Main menu" on your sub-theme's settings form, deleting this PHP
-          // code block, and, instead, using the "Menu block" module.
-          // @see https://drupal.org/project/menu_block
-          print theme('links__system_main_menu', array(
-            'links' => $main_menu,
-            'attributes' => array(
-              'class' => array('links', 'inline', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => t('Main menu'),
-              'level' => 'h2',
-              'class' => array('element-invisible'),
-            ),
-          )); ?>
-        </nav>
-      <?php endif; ?>
-
-      <?php print render($page['navigation']); ?>
-
-    </div>
+   
 
     <?php
       // Render the sidebars to see if there's anything in them.
