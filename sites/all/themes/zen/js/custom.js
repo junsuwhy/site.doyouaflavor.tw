4d8744889b4de03717d3b51c9c08514938409f42
junsuwhy <junsuwhy@gmail.com>
2015-03-29 17:37:20 +0800

    Update

--- sites/all/themes/zen/js/custom.js
+++ sites/all/themes/zen/js/custom.js
@@ -1,5 +1,29 @@
 jQuery(document).ready(function($){
+  $(window).scroll(onRoll);
 
-  console.log($);
+  function onRoll(){
+    $('.region-header').css({'background-position-y': $(window).scrollTop()});
+  }
+
+  console.log($('.slick-carousel'));
+  $('.pane-carousel .view-content').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    arrows: false,
+    swipe: true,
+  });
+  $('.pane-home-master .view-content').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    arrows: false,
+    swipe: true,
+    adaptiveHeight: true
+  });
+  $('.pane-carousel .slick-list').before($('.pane-carousel .slick-dots'));
+  $('.pane-home-master .slick-list').before($('.pane-home-master .slick-dots'));
+  
+
 });
-
+  
