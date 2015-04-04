9d92aaf23fd20ce4115be21a3c50f84f5e5760d8
junsuwhy <junsuwhy@gmail.com>
2015-04-04 10:24:50 +0800

    Modify css

--- sites/all/themes/zen/js/custom.js
+++ sites/all/themes/zen/js/custom.js
@@ -1,13 +1,54 @@
 jQuery(document).ready(function($){
-  $(window).scroll(onRoll);
+  
+  $w = $(window);
+
+
+
+  $ppt = $('.pane-home-products');
+  $rh = $('.region-header');
+
+  $hlmw = $('.header-logo-menu-wrapper');
+  hlmwp = $hlmw.css('position');
+  hlmwh = $hlmw.height()+parseInt($hlmw.css('padding-top').substr(0,2));
+  $main = $("#main");
+  $hd = $('.header__region');
+
+  onResize();
+  $w.resize(onResize);
+
+  onRoll();
+  $w.scroll(onRoll);
+
+  
 
   function onRoll(){
     // var y = $('.pane-home-products').position().top;
-    $('.region-header').css({'background-position-y': $(window).scrollTop()});
+    $rh.css({'background-position-y': $w.scrollTop()});
+    $ppt.css({'background-position-y': $w.scrollTop()-$ppt.offset().top});
+
+    if($w.scrollTop() < $hd.height()){
+      if(hlmwp == 'fixed'){
+        $hlmw.css({'position':'inherit'});
+        hlmwp = 'inherit';
+        $main.css({'margin-top':0});
+      }
+    }else{
+      if(hlmwp == 'inherit'){
+        $hlmw.css({'position':'fixed'});
+        hlmwp = 'fixed';
+        $main.css({'margin-top':hlmwh});
+      }
+    }
+    
     // $('.pane-home-products').css({'background-position-y': $(window).scrollTop()-y});
   }
 
-  console.log($('.slick-carousel'));
+  function onResize(){
+    $ppt.css({'height': $w.height()});
+    $rh.css({'height': $w.height()});
+    
+  }
+
   $('.pane-carousel .view-content').slick({
     dots: true,
     infinite: true,
@@ -23,7 +64,7 @@ jQuery(document).ready(function($){
     speed: 300,
     arrows: false,
     swipe: true,
-    adaptiveHeight: true,
+    // adaptiveHeight: false,
     autoplay: true,
     autoplaySpeed: 2000,
   });
@@ -33,16 +74,19 @@ jQuery(document).ready(function($){
 
 
   $('.home-scroll-button').click(function(){
-    var scrollAmount = $('.navigation-wrapper').position().top+$('#navigation').position().top;
+    var scrollAmount = $hlmw.offset().top;
     $('body').animate({scrollTop: scrollAmount},1000);
   })
 
   if($('body.front').length==1){
     $('.menu-423 a').click(function(){
-      var scrollAmount = $('#products').position().top;
+      var scrollAmount = $('#products').offset().top;
       $('body').animate({scrollTop: scrollAmount},1000);  
     })
   }
+
+  $bb3 = $('#block-block-3');
+  $bb3.css('padding-top',($w.height()-$bb3.height())/2-10)+'px 0';
   
 
 });
