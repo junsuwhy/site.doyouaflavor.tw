df67d545b0e1c4acbca17a34ef251f9acbc65fb5
junsuwhy <junsuwhy@gmail.com>
2015-07-25 22:43:22 +0800

    Add mixpanel

---
+++ sites/all/themes/zen/js/custom_mixpanel.js
@@ -0,0 +1,69 @@
+(function($){
+    $(function(){
+
+      if(!window.location.search.split("notrack=")[1]){
+        mixpanel.track("visit");
+
+        var stop = {};
+        stop.global_time = 0;
+        stop.once_time = 0;
+        stop.countId = setInterval(function(){
+          stop.global_time++;
+        },1000);
+
+        $('a').click(function(){
+          var _href = $(this).attr('href');
+          console.log(_href);
+          mixpanel.track("outbound",{
+            "href":_href,
+            "second":stop.global_time
+          });
+        });
+        window.onbeforeunload = function (e) {
+          // var message = "Your confirmation message goes here.",
+          // e = e || window.event;
+          // For IE and Firefox
+          // if (e) {
+            // e.returnValue = message;
+            
+          // }
+
+          // For Safari
+          mixpanel.track("leave",{
+            "second":stop.global_time
+          });
+          // return message;
+        };
+
+        var focus = {};
+
+        $('input[type="text"],textarea').focus(function(){
+          focus.name = $(this).attr('name');
+          focus.time = 0;
+          focus.countId = setInterval(function(){
+            focus.time++;
+          },1000);
+        });
+
+        $('input[type="text"],textarea').blur(function(){
+          // console.log($(this));
+          // var val = $(this).val();
+          // console.log(val);
+          // var fld_name = $(this).attr('name');
+          // console.log(fld_name);
+          output = {
+            "value": $(this).val(),
+            "field": $(this).attr('name')
+          }
+          if(focus.name == $(this).attr('name')){
+            clearInterval(focus.countId);
+            output.stop_second = focus.time;
+          }
+          mixpanel.track("input",output);
+          
+        })
+
+      }
+
+  });
+})(jQuery);
